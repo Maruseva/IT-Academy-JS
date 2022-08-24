0e99eb3245fdf6d90014fb06b234b904bf29514d
maruseva <maruseva@yandex.ru>
2022-08-24 11:04:08 +0400

    fix: B3+ add work with "-"

--- B3+/script.js
+++ B3+/script.js
@@ -2,7 +2,7 @@ function calc (str) {
 
     let arr;
     let arrSolution = [];
-
+   
     if(typeof str === 'string') {
 
         arr = str.split("");
@@ -14,17 +14,19 @@ function calc (str) {
 
     if(arr.indexOf("(") !== -1) {
 
-        let parentheses = arr.slice(arr.indexOf("(") + 1, arr.indexOf(")")); 
-        let parenthesesSolution = calc(parentheses);
+        const parentheses = arr.slice(arr.indexOf("(") + 1, arr.indexOf(")")); 
+        const parenthesesSolution = calc(parentheses);
 
-        arr.splice(arr.indexOf("("), (arr.indexOf(")") - arr.indexOf("(") + 1), parenthesesSolution[0]);
+        arr.splice(arr.indexOf("("), (arr.indexOf(")") - arr.indexOf("(") + 1), parenthesesSolution);
         calc(arr);
     };
 
     for (let i = 0; i < arr.length; i++) {
 
-        if ((i > 0) && (arr[i] !== "-" && arr[i] !== "+" && arr[i] !== "*" && arr[i] !== "/") 
-        && (arrSolution[arrSolution.length-1] !== "-" && arrSolution[arrSolution.length-1] !== "+" && arrSolution[arrSolution.length-1] !== "*" && arrSolution[arrSolution.length-1] !== "/")) {
+        if (((arr[i] !== "-" && arr[i] !== "+" && arr[i] !== "*" && arr[i] !== "/") && (arrSolution[arrSolution.length-1] === "-") &&
+        (arrSolution[arrSolution.length-2] === "-" || arrSolution[arrSolution.length-2] === "+" || arrSolution[arrSolution.length-2] === "*" || arrSolution[arrSolution.length-2] === "/" || arrSolution[arrSolution.length-2] === undefined)) ||
+        ((i > 0) && (arr[i] !== "-" && arr[i] !== "+" && arr[i] !== "*" && arr[i] !== "/") 
+        && (arrSolution[arrSolution.length-1] !== "-" && arrSolution[arrSolution.length-1] !== "+" && arrSolution[arrSolution.length-1] !== "*" && arrSolution[arrSolution.length-1] !== "/"))) {
 
             arrSolution[arrSolution.length-1] += arr[i];
 
@@ -34,26 +36,20 @@ function calc (str) {
         };
     };
 
-    if (arrSolution[0] === "-") {
-
-        arrSolution[1] = arrSolution[0] + arrSolution[1];
-        arrSolution.shift();
-    };
-
     if (arrSolution.includes("*") || arrSolution.includes("/")) {
 
         for (let i = 0; i < arrSolution.length; i++) {
 
             if (arrSolution[i] === "*") {
     
-                let result = arrSolution[i-1] * arrSolution[i+1];
+                const result = arrSolution[i-1] * arrSolution[i+1];
     
                 arrSolution.splice(i-1, 3, result)
             };
     
             if (arrSolution[i] === "/") {
     
-                let result = arrSolution[i-1] / arrSolution[i+1];
+                const result = arrSolution[i-1] / arrSolution[i+1];
     
                 arrSolution.splice(i-1, 3, result)
             };
@@ -66,20 +62,20 @@ function calc (str) {
 
             if (arrSolution[i] === "-") {
     
-                let result = arrSolution[i-1] - arrSolution[i+1];
+                const result = arrSolution[i-1] - arrSolution[i+1];
     
                 arrSolution.splice(i-1, 3, result)
             };
     
             if (arrSolution[i] === "+") {
     
-                let result = Number(arrSolution[i-1]) + Number(arrSolution[i+1]);
+                const result = Number(arrSolution[i-1]) + Number(arrSolution[i+1]);
     
                 arrSolution.splice(i-1, 3, result)
             };
         };
     };
 
-    return arrSolution;
+    return arrSolution[0];
 };
-console.log(calc("2*(-3+1)")) 
+console.log(calc("2*(-3.5+1)")) 
