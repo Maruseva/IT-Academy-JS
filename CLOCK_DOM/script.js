548faaba531c82dd8f3a5cc13fcf5ee5cdd36268
maruseva <maruseva@yandex.ru>
2022-08-31 19:27:05 +0300

    add Timeout

--- CLOCK_DOM/script.js
+++ CLOCK_DOM/script.js
@@ -1,6 +1,19 @@
 const button = document.getElementById('button');
 button.addEventListener("click", showClock);
 
+const coefWidthHour = 25; /* ширина часовой стрелки 1/25 диаметра циферблата*/
+const coefWidthMinute = 35; /* ширина минутной стрелки 1/35 диаметра циферблата*/
+const coefWidthSecond = 50; /* ширина секундной стрелки 1/50 диаметра циферблата*/
+const coefHeightHour = 0.25; /* длина часовой стрелки 25% диаметра циферблата*/
+const coefHeightMinute = 0.3; /* длина минутной стрелки 30% диаметра циферблата*/
+const coefHeightSecond = 0.45; /* длина секундной стрелки 45% диаметра циферблата*/
+const coefFontSize = 12; /* размер шрифта в 12 раз меньше размера диаметра циферблата*/
+const coefHourCircle = 10; /* диаметр цифр в 10 раз меньше даиметра циферблата*/
+const coefDegrees = 6; /* градусов на одном делении циферблата, 360/60*/ 
+const coefClockHands = 0.1; /*короткий край стрелки 10% ее длины*/
+
+let angl = 30; /*угол в одном часе 360/12, расстояние между цифрами*/
+
 function showClock() {
 
     const text = document.getElementById('text').value;
@@ -13,56 +26,119 @@ function showClock() {
 
     inputs.style.display = "none";
     clockFace.style.cssText = `background: #d5c06d; border-radius: 50%; width: ${text}px; height: ${text}px; `;
-    hourHand.style.width = `${text/25}px`; /* ширина часовой стрелки 1/25 диаметра циферблата*/
-    hourHand.style.height = `${text/2*0.5}px`; /* длина часовой стрелки 50% радиуса циферблата*/
-    minuteHand.style.width = `${text/35}px`; /* ширина минутной стрелки 1/35 диаметра циферблата*/
-    minuteHand.style.height = `${text/2*0.6}px`; /* длина минутной стрелки 60% радиуса циферблата*/
-    secondHand.style.width = `${text/50}px`; /* ширина секундной стрелки 1/50 диаметра циферблата*/
-    secondHand.style.height = `${text/2*0.9}px`; /* длина секундной стрелки 90% радиуса циферблата*/
-
+    hourHand.style.width = `${text/coefWidthHour}px`; 
+    hourHand.style.height = `${text*coefHeightHour}px`;
+    minuteHand.style.width = `${text/coefWidthMinute}px`; 
+    minuteHand.style.height = `${text*coefHeightMinute}px`; 
+    secondHand.style.width = `${text/coefWidthSecond}px`; 
+    secondHand.style.height = `${text*coefHeightSecond}px`; 
+    
     for(let i = 0; i < clockHands.length; i++) {
 
         clockHands[i].style.background = `black`;
         clockHands[i].style.borderRadius = `50px`;
         clockHands[i].style.position = `absolute`;
         clockHands[i].style.left = `${clockFace.offsetWidth/2+clockFace.offsetLeft-clockHands[i].offsetWidth/2}px`;
-        clockHands[i].style.top = `${clockFace.offsetHeight/2-clockHands[i].offsetHeight+clockHands[i].offsetHeight*0.1}px`; /*короткий край стрелки 10% ее длины*/
-        // clockHands[i].style.transform = `rotate(${360/60}deg)`; /*на циферблате 60 делений */
-        // clockHands[i].style.transformOrigin = `50% 90% 0`;
+        clockHands[i].style.top = `${clockFace.offsetHeight/2+clockFace.offsetTop-clockHands[i].offsetHeight+clockHands[i].offsetHeight*coefClockHands}px`; 
+        clockHands[i].style.transformOrigin = `50% 90% 0`;
         clockHands[i].style.zIndex = `2`;
-    };
+    }
 
     for(let i = 12; i > 0; i--) { /*на циферблате 12 часов*/
 
-        let hour = document.createElement('div');
-        let span = document.createElement('span');
+        const hour = document.createElement('div');
+        const span = document.createElement('span');
 
         span.innerText = `${i}`;
         hour.appendChild(span);
-
-        
+        clockFace.appendChild(hour);
 
         hour.style.background = `#3f9e3f`;
         hour.style.color = `black`;
         hour.style.textAlign = `center`;
-        hour.style.fontSize = `${text/12}px`; /* размер шрифта в 12 раз меньше размера диаметра циферблата*/
+        hour.style.fontSize = `${text/coefFontSize}px`; 
         hour.style.borderRadius = `50%`;
-        hour.style.width = `${text/10}px`; /* диаметр цифр в 10 раз меньше даиметра циферблата*/
-        hour.style.height = `${text/10}px`; /* диаметр цифр в 10 раз меньше даиметра циферблата*/
+        hour.style.width = `${text/coefHourCircle}px`; 
+        hour.style.height = `${text/coefHourCircle}px`;
         hour.style.position = `absolute`;
+        
+        angl += -30;
 
-        let left = `${clockFace.offsetWidth/2+clockFace.offsetLeft-hour.offsetWidth/2}px`;
-        let top = `${text/12}px`;
-        hour.style.left = `${left}`;
-        hour.style.top = `${top}`;
+        const clockFaceCenterX = clockFace.offsetLeft+clockFace.offsetWidth/2;
+        const clockFaceCenterY = clockFace.offsetTop+clockFace.offsetHeight/2;
 
-        clockFace.appendChild(hour);
+        const hourCenterX = clockFaceCenterX+text/2*0.8*Math.sin(angl/180*Math.PI);
+        const hourCenterY = clockFaceCenterY-text/2*0.8*Math.cos(angl/180*Math.PI);
+
+        hour.style.left = Math.round(hourCenterX-hour.offsetWidth/2)+'px';
+        hour.style.top = Math.round(hourCenterY-hour.offsetHeight/2)+'px';
+    }
+
+    setInterval(updateTime,1000);
+
+    function updateTime() {
+
+        const newDate = new Date();
+        const newDateStr = formatDateTime(newDate);
+        console.log (newDateStr);
+        const date = document.getElementById('date');
+
+        date.innerHTML = newDateStr;
+        date.style.fontSize = `${text/coefFontSize}px`;
+        date.style.position = `absolute`;
+        date.style.left = `${clockFace.offsetWidth/2+clockFace.offsetLeft-date.offsetWidth/2}px`;
+        date.style.top = `${clockFace.offsetHeight/4+clockFace.offsetTop-date.offsetHeight/2}px`;
+        
+        function formatDateTime(dt) {
+
+            const hours=dt.getHours();
+            const minutes=dt.getMinutes();
+            const seconds=dt.getSeconds();
+            return str0l(hours,2) + ':' + str0l(minutes,2) + ':' + str0l(seconds,2);
+        };
+
+        function str0l(val,len) {
+            let strVal=val.toString();
+            while ( strVal.length < len )
+                strVal='0'+strVal;
+            return strVal;
+        };
     };
 
+    let startDate = new Date ();
+    startDate.setHours(0);
+    startDate.setMinutes(0);
+    startDate.setSeconds(0);
 
+    let degrees = 0;
+
+    setTimeout(moveSecond,4);
+    setTimeout(moveMinute,4);
+    setTimeout(moveHour,4);
     
+    setInterval(moveSecond, 1000);
 
+    function moveSecond () {
 
-}
+        degrees = (new Date() - startDate)/1000*coefDegrees; 
+        secondHand.style.transform = `rotate(${degrees += coefDegrees}deg)`; 
+    };
+
+    setInterval(moveMinute, 60000); /* в минуте 60000 миллисекунд */
+
+    function moveMinute () {
+
+        degrees = (new Date() - startDate)/60000*coefDegrees; 
+        minuteHand.style.transform = `rotate(${degrees += coefDegrees}deg)`;
+    };
+
+    setInterval(moveHour, 720000); /*часовая стрелка проходит одно деление за 12 минут(60/5), а это 12*60000=720000*/
+
+    function moveHour () {
+
+        degrees = (new Date() - startDate)/720000*coefDegrees; 
+        hourHand.style.transform = `rotate(${degrees += coefDegrees}deg)`;
+    };
+};
 
 
